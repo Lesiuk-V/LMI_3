8de0c1b5c6b003bbbaa69ffa7fcf36ebd472e449
Vasya Lesiuk <lenono526@gmail.com>
2020-11-13 21:23:53 +0200

    Добавьте файлы проекта.

---
+++ WpfApp1/Human.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+
+namespace WpfApp1
+{
+    public class Human
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public int Age { get; set; }
+
+        public Human(string name = "unknow", string surname = "unknow", int age = 0)
+        {
+            Name = name;
+            Surname = surname;
+            Age = age;
+        }
+       
+    }
+}
---
+++ WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public bool start = true;
+        private ObservableCollection<Human> human;
+       // public ObservableCollection<Human> data = new ObservableCollection<Human>();
+        public MainWindow()
+        {
+            InitializeComponent();
+            human = new ObservableCollection<Human>()
+            {
+                 new Human(){Name="Willa",Surname="Cather", Age=10},
+
+                 new Human(){Name= "Isak", Surname= "Dinesen", Age = 22}
+            };
+            lstNames.ItemsSource = human;
+        }
+        public MainWindow(bool o)
+        {
+            start = false;
+        }
+        //public ObservableCollection<Human> GetData() 
+        //{
+        //    //data.Add(new Human("Willa", "Cather", 18));
+        //    //data.Add(new Human("Isak", "Dinesen", 22));
+        //    //data.Add(new Human("Victor", "Hugo", 10));
+        //    //data.Add(new Human("Jules", "Verne", 20));
+
+        //    //return data;
+        //}
+        private void Add_Click(object sender, RoutedEventArgs e)
+        {
+            human.Add(new Human(NameTextbox.Text, SurnameTextbox.Text, Convert.ToInt32(AgeTextBox.Text)));
+            DataContext = human;
+        }
+
+        private void Delete_Click(object sender, RoutedEventArgs e)
+        {
+            if (DeleteByIdTextBox.Text != string.Empty)
+            {
+                var deletePerson = (from p in human
+                                    where p.Name == DeleteByIdTextBox.Text
+                                    select p).FirstOrDefault();
+                human.Remove(deletePerson);
+                DeleteByIdTextBox.Text = string.Empty;
+            }
+        }
+
+    }
+}
