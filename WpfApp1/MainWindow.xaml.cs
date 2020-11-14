11e380896ccc74e7ba4424ac3778e0340e4331a0
Vasya Lesiuk <lenono526@gmail.com>
2020-11-14 10:22:22 +0200

    .

--- WpfApp1/MainWindow.xaml.cs
+++ WpfApp1/MainWindow.xaml.cs
@@ -21,33 +21,19 @@ namespace WpfApp1
     /// </summary>
     public partial class MainWindow : Window
     {
-        public bool start = true;
         private ObservableCollection<Human> human;
-       // public ObservableCollection<Human> data = new ObservableCollection<Human>();
         public MainWindow()
         {
             InitializeComponent();
             human = new ObservableCollection<Human>()
             {
-                 new Human(){Name="Willa",Surname="Cather", Age=10},
-
-                 new Human(){Name= "Isak", Surname= "Dinesen", Age = 22}
+                 new Human(){Name = "Willa", Surname = "Cather", Age = 10},
+                 new Human(){Name = "Isak", Surname = "Dinesen", Age = 22},
+                 new Human(){Name = "Victor", Surname = "Hugo", Age = 10},
+                 new Human(){Name = "Jules", Surname = "Verne", Age = 20}
             };
-            lstNames.ItemsSource = human;
+            NameList.ItemsSource = human;
         }
-        public MainWindow(bool o)
-        {
-            start = false;
-        }
-        //public ObservableCollection<Human> GetData() 
-        //{
-        //    //data.Add(new Human("Willa", "Cather", 18));
-        //    //data.Add(new Human("Isak", "Dinesen", 22));
-        //    //data.Add(new Human("Victor", "Hugo", 10));
-        //    //data.Add(new Human("Jules", "Verne", 20));
-
-        //    //return data;
-        //}
         private void Add_Click(object sender, RoutedEventArgs e)
         {
             human.Add(new Human(NameTextbox.Text, SurnameTextbox.Text, Convert.ToInt32(AgeTextBox.Text)));
@@ -56,13 +42,13 @@ namespace WpfApp1
 
         private void Delete_Click(object sender, RoutedEventArgs e)
         {
-            if (DeleteByIdTextBox.Text != string.Empty)
+            if (DeleteTextBox.Text != string.Empty)
             {
                 var deletePerson = (from p in human
-                                    where p.Name == DeleteByIdTextBox.Text
+                                    where p.Name == DeleteTextBox.Text
                                     select p).FirstOrDefault();
                 human.Remove(deletePerson);
-                DeleteByIdTextBox.Text = string.Empty;
+                DeleteTextBox.Text = string.Empty;
             }
         }
 
